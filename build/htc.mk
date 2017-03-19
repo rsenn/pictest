b2649ca311ead058b9c317d3b3e768ddccd8b2cf
Roman Senn <roman.l.senn@gmail.com>
2017-03-19 20:02:47 +0000

    ...

--- build/htc.mk
+++ build/htc.mk
@@ -9,6 +9,7 @@ VERSION_PATCH = 1
 COMPILER := htc
 
 -include build/vars.mk
+-include build/targets.mk
 
 VERSION = $(VERSION_MAJOR).$(VERSION_MINOR).$(VERSION_PATCH)
 
@@ -16,34 +17,54 @@ PROGRAMFILES = C:/Program Files (x86)
 
 OS = $(shell uname -o)
 $(info OS: $(OS))
+$(info CHIP: $(CHIP))
 #CC = $(shell which picc 2>/dev/null)
 
-ifneq ($(CHIP),$(CHIP:18%=%))
+
+
+ifneq ($(CHIP),$(subst 18f,,$(CHIP)))
 COMPILER_NAME = picc18
 COMPILER_DIR = picc-18/pro
-CCVER = 9.66
+CCVER := 9.66
 PIC18 = true
 DEFINES += __PICC18__=1
 else
 COMPILER_NAME = picc
-COMPILER_DIR = picc
-CCVER = 9.83
+COMPILER_DIR = PICC
+#CCVER = PRO/9.60
+#CCVER = 9.83
+CCVER := 9.71a
 endif
+
+ifneq ($($(subst -,_,$(PROGRAM))_CCVER),)
+CCVER := $($(subst -,_,$(PROGRAM))_CCVER)
+endif
+
+
+ifeq ($(COMPILER_NAME),picc18)
+CCVER := 9.66
+endif
+
 
 ifeq ($(CCDIR),)
 ifeq ($(OS),GNU/Linux)
-CCDIR = /opt/hitech/$(COMPILER_DIR)/$(CCVER)
+CCDIR := /opt/hitech/$(COMPILER_DIR)/$(CCVER)
 else
-CCDIR = $(PROGRAMFILES)/HI-TECH Software/$(COMPILER_DIR)/$(CCVER)
+CCDIR := $(PROGRAMFILES)/HI-TECH Software/$(COMPILER_DIR)/$(CCVER)
 endif
 endif
 
 
 ifneq ($(CCDIR),)
-CC = "$(CCDIR)/bin/$(COMPILER_NAME)"
+CC := "$(CCDIR)/bin/$(COMPILER_NAME)"
 else
 CC = $(COMPILER_NAME)
 endif
+
+$(info CC: $(CC))
+$(info CCVER: $(CCVER))
+
+$(info COMPILER_NAME: $(COMPILER_NAME))
 
 #ifeq ($(strip $(CC)),)
 #CC = picc
@@ -57,12 +78,12 @@ LD = $(CC)
 #RM = del /f
 DISTFILES = Makefile build/htc.mk build/sdcc.mk
 
-OPT = space
+OPT = speed
 
 #DEFINES += HI_TECH_C=1
 
-COMMON_FLAGS += $($(PROGRAM)_DEFS)
-SOURCES := $($(PROGRAM)_SOURCES) $(COMMON_SOURCES)
+SOURCES := $($(subst -,_,$(PROGRAM))_SOURCES) $(COMMON_SOURCES)
+COMMON_FLAGS += $($(subst -,_,$(PROGRAM))_DEFS)
 DEPENDS := $(SOURCES:%.c=$(OBJDIR)%.dep)
 P1OBJS = $(SOURCES:%.c=$(OBJDIR)%.p1)
 ASSRCS = $(SOURCES:%.c=$(OBJDIR)%.as)
@@ -70,44 +91,47 @@ ASSRCS = $(SOURCES:%.c=$(OBJDIR)%.as)
 COMMON_FLAGS += -N127
 #COMMON_FLAGS += --scandep
 
-#COMMON_FLAGS += --runtime="default,+clear,+init,-keep,+osccal,+download,-resetbits,+clib"
-#COMMON_FLAGS += --runtime="default,-clear,+init,+keep,-osccal,+download,+resetbits,+clib"
-COMMON_FLAGS += --runtime="default,+init,+download,+clib"
+#COMMON_FLAGS += --runtime=default
+#COMMON_FLAGS += --runtime=default
 
-ifeq ($(BUILD_TYPE),debug)
-DO_DEPENDS := 1
+ifneq ($(PIC18),true)
+STACKCALL = ,+stackcall
 endif
+
+COMMON_FLAGS += --runtime=default$(STACKCALL)
+
+ifneq ($(CCVER),9.60)
 
 ifeq ($(OPT),speed)
-OPT_SPEED = ,+speed,-space,9
+OPT_SPEED = ,-space,+speed
 endif
 ifeq ($(OPT),space)
-OPT_SPEED = ,+speed,-space,9
+OPT_SPEED = ,-speed,+space
 endif
+endif
+OPTLEVEL := 9
+OPT_SPEED := $(OPT_SPEED),$(OPTLEVEL)
 
 ifeq ($(BUILD_TYPE),debug)
-
-ifeq ($(PIC18),true)
-OPT_DEBUG :=
-else
-OPT_DEBUG := ,+debug
-endif
-DEBUG := 1
+ ifeq ($(PIC18),true)
+  OPT_DEBUG :=
+ else
+#  OPT_DEBUG := ,+debug
+ endif
+ _DEBUG := 1
 endif
 
 #COMMON_FLAGS +=  -V
 
-ifeq ($(DEBUG),1)
-COMMON_FLAGS += -G 
-COMMON_FLAGS +=  --opt=none
+ifeq ($(_DEBUG),1)
+COMMON_FLAGS += -G --debugger=pickit2
+COMMON_FLAGS += --opt=default$(OPT_DEBUG)
 else
 COMMON_FLAGS += --opt="default,+asm$(OPT_SPEED)"
 endif
 
-COMMON_FLAGS += --debugger=icd2
-
 COMMON_FLAGS += --double=32 
-#COMMON_FLAGS += --float=24
+#COMMON_FLAGS += --float=32
 
 COMMON_FLAGS += --warn=3
 #COMMON_FLAGS += --codeoffset=$(CODEOFFSET)
@@ -126,7 +150,7 @@ _CPPFLAGS += \
 	-DVERSION_MINOR=$(VERSION_MINOR) \
 	-DVERSION_PATCH=$(VERSION_PATCH)
 
-CFLAGS = -v --mode=pro --chip=$(chipl) $(COMMON_FLAGS)
+CFLAGS = -q --chip=$(chipl) $(COMMON_FLAGS)
 
 _LDFLAGS += --summary="default,-psect,-class,+mem,-hex"
 
@@ -138,13 +162,9 @@ PM3CMD = "$$PROGRAMFILES"/Microchip/MPLAB\ IDE/Programmer\ Utilities/PM3Cmd/PM3C
 
 COFFILE = $(subst .hex,.cof,$(HEXFILE))
 
-ifeq ($(DO_DEPENDS),1)
 DEPS := $(CC) --scandep $(SOURCES)
 
 $(info DEPS: $(DEPS))
-else
-DEPENDS := 
-endif
 
 #-include build/vars.mk
 
@@ -162,7 +182,7 @@ dist:
 	cp -rvf $(DISTFILES) $(PROGRAM)-$(VERSION)
 	tar -cvzf $(PROGRAM)-$(VERSION).tar.gz $(PROGRAM)-$(VERSION)
 
-$(HEXFILE): $(P1OBJS)
+$(HEXFILE): $(P1OBJS) | $(BUILDDIR) $(OBJDIR)
 	$(RM) $(HEXFILE) $(COFFILE)
 	$(LD) $(LDFLAGS) -m$(BUILDDIR)$(PROGRAM)_$(BUILD_TYPE)_$(MHZ)mhz_$(KBPS)kbps_$(SOFTKBPS)skbps.map -o$@ $^
 	#(cd bin; $(LD) $(LDFLAGS) -m$(BUILDDIR:bin/%=%)$(PROGRAM)_$(BUILD_TYPE)_$(MHZ)mhz_$(KBPS)kbps_$(SOFTKBPS)skbps.map -o$(@:bin/%=%) $(^:%=../%))
@@ -171,12 +191,13 @@ $(HEXFILE): $(P1OBJS)
 
 $(P1OBJS): $(OBJDIR)%.p1: %.c
 	(cd obj; $(CC) --pass1 $(CFLAGS) $(CPPFLAGS:-I%=-I../%) --outdir=$(OBJDIR:obj/%/=%) ../$<)
+#	$(CC) --pass1 $(CFLAGS) $(CPPFLAGS) -o$(<:%.c=$(BUILDDIR)%_$(BUILD_TYPE)_$(MHZ)mhz_$(KBPS)kbps_$(SOFTKBPS)skbps.p1) $<
 
-ifeq ($(DO_DEPENDS),1)
 $(DEPENDS): $(OBJDIR)%.dep: %.c
 	(cd obj; $(CC) --scandep $(CFLAGS) $(CPPFLAGS:-I%=-I../%) --outdir=$(OBJDIR:obj/%/=%) ../$<); \
-	 sed  '/:/d; s,[\\\\],/,g ; s|^\.\./||;  /[ ()]/ s,.*,"&",' <"$@" | sed ':lp; N; $$! { b lp }; s,\n, ,g; s|^|$(patsubst %.c,$(OBJDIR)/%.p1,$(notdir $<)): |;  s|/\+|/|g'  >"$(@:%.dep=%.d)"
-endif
+	 sed  '/:/d; s,[\\\\],/,g ; s|^\.\./||;  /[ ()]/ s,.*,"&",' <"$@" | sed ':lp; N; $$! { b lp }; s,\n, ,g; s|^|$(patsubst %.c,$(OBJDIR)/%.p1,$(notdir $<)): |;  s|/\+|/|g ; s|\r$$||'  >"$(@:%.dep=%.d)"
+
+#	$(CC) --pass1 $(CFLAGS) $(CPPFLAGS) -o$(<:%.c=$(BUILDDIR)%_$(BUILD_TYPE)_$(MHZ)mhz_$(KBPS)kbps_$(SOFTKBPS)skbps.p1) $<
 
 $(ASSRCS): $(OBJDIR)%.as: %.c
 	$(CC) -S $(CFLAGS) $(CPPFLAGS) --outdir=$(OBJDIR:%/=%) $<
@@ -187,8 +208,5 @@ ifneq ($(CCDIR),)
 prototypes: CPPFLAGS += -I'$(CCDIR)/include'
 endif
 
-ifeq ($(DO_DEPENDS),1)
 -include $(DEPENDS:%.dep=%.d)
-endif
-
 -include build/common.mk
