a7582f0e1f3705a4bafa7711c44d87394f2214bd
Roman <roman.l.senn@gmail.com>
2020-01-28 17:46:38 +0100

    ...

--- gen-mk-mplab.sh
+++ gen-mk-mplab.sh
@@ -28,7 +28,7 @@ BUILD_TYPES='debug release'
 : ${PROGRAMS:='blinktest pictest serialtest'}
 
 for PROGRAM_NAME in $PROGRAMS; do 
- (eval "SOURCES=\$(set -- $SOURCES; ls -d \$@ 2>/dev/null)"
+  (eval "SOURCES=\$(set -- $SOURCES; echo \$(ls -d \$@) 2>/dev/null)"
   echo "${nl}SOURCES='"$SOURCES"'${nl}" 1>&2
 
   for CHIP in $CHIPS;  do
@@ -37,7 +37,7 @@ for PROGRAM_NAME in $PROGRAMS; do
       do
         eval "SOURCES=\$(set -- $SOURCES; echo \$(ls -d \$@))"
 
-      eval "(set -x;	genmakefile -t \$COMPILER -m mplab $@ -I. -Ilib -Isrc \$SOURCES -DHAVE_COMPARATOR=1 -DSOFTSER_TIMER=2 -DUART_BAUD=38400 -DUSE_SOFTPWM=1 -DUSE_SOFTSER=1 -DUSE_TIMER0=1 -DUSE_TIMER1=1 -DUSE_TIMER2=1 -D_XTAL_FREQ=20000000 --create-bins --no-create-libs --$BUILD_TYPE --chip=${CHIP} -o build/mplab/$PROGRAM_NAME-$CHIP-$COMPILER-$BUILD_TYPE.mcp >&genmakefile-$COMPILER-$BUILD_TYPE.log) 2>&10"
+      eval "(set -x;	genmakefile -t \$COMPILER -m mplab $@ -I. -Ilib -Isrc "$SOURCES" -DHAVE_COMPARATOR=1 -DSOFTSER_TIMER=2 -DUART_BAUD=38400 -DUSE_SOFTPWM=1 -DUSE_SOFTSER=1 -DUSE_TIMER0=1 -DUSE_TIMER1=1 -DUSE_TIMER2=1 -D_XTAL_FREQ=20000000 --create-bins --no-create-libs --$BUILD_TYPE --chip=${CHIP} -o build/mplab/$PROGRAM_NAME-$CHIP-$COMPILER-$BUILD_TYPE.mcp >&genmakefile-$COMPILER-$BUILD_TYPE.log) 2>&10"
       done
     done
   done)
