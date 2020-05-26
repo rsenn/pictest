9ecbfcf27e37216819ebf3713b994d5f369d1004
Roman <roman.l.senn@gmail.com>
2020-05-27 00:31:43 +0200

    ...

---
+++ build/mplabx-pic16/seg7test_16f876a_xc8.X/nbproject/Makefile-variables.mk
@@ -0,0 +1,20 @@
+#
+# Generated - do not edit!
+#
+# NOCDDL
+#
+CND_BASEDIR=`pwd`
+# debug configuration
+CND_ARTIFACT_DIR_debug=dist/debug/production
+CND_ARTIFACT_NAME_debug=seg7test_16f876a_xc8.X.production.hex
+CND_ARTIFACT_PATH_debug=dist/debug/production/seg7test_16f876a_xc8.X.production.hex
+CND_PACKAGE_DIR_debug=${CND_DISTDIR}/debug/package
+CND_PACKAGE_NAME_debug=seg7test16f876axc8.x.tar
+CND_PACKAGE_PATH_debug=${CND_DISTDIR}/debug/package/seg7test16f876axc8.x.tar
+# release configuration
+CND_ARTIFACT_DIR_release=dist/release/production
+CND_ARTIFACT_NAME_release=seg7test_16f876a_xc8.X.production.hex
+CND_ARTIFACT_PATH_release=dist/release/production/seg7test_16f876a_xc8.X.production.hex
+CND_PACKAGE_DIR_release=${CND_DISTDIR}/release/package
+CND_PACKAGE_NAME_release=seg7test16f876axc8.x.tar
+CND_PACKAGE_PATH_release=${CND_DISTDIR}/release/package/seg7test16f876axc8.x.tar
