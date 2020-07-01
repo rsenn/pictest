2822be0a2abade916ebe810bab894969d34aaf37
Roman <roman.l.senn@gmail.com>
2020-07-01 14:36:49 +0200

    ...

---
+++ program.sh
@@ -0,0 +1,1 @@
+scripts/ipecmd.sh -TPPK3 -E -M -P${CHIP:-16f876a} -F"$1" -OL
---
+++ scripts/ipecmd.sh
@@ -0,0 +1,2 @@
+#!/bin/sh
+exec env jdkhome=/opt/microchip/mplabx/v5.30/sys/java/jre1.8.0_181 /opt/microchip/mplabx/v5.30/mplab_platform/mplab_ipe/ipecmd.sh "$@"
