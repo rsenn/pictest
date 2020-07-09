a9567eb44cb2ec05345420b550b3954d461a0a6c
Roman <roman.l.senn@gmail.com>
2020-07-09 08:38:25 +0200

    ...

---
+++ build/mplabx-pic16/serialtest_16f876a_xc8.X/nbproject/Makefile-variables.mk
@@ -0,0 +1,20 @@
+#
+# Generated - do not edit!
+#
+# NOCDDL
+#
+CND_BASEDIR=`pwd`
+# debug configuration
+CND_ARTIFACT_DIR_debug=dist/debug/production
+CND_ARTIFACT_NAME_debug=serialtest_16f876a_xc8.X.production.hex
+CND_ARTIFACT_PATH_debug=dist/debug/production/serialtest_16f876a_xc8.X.production.hex
+CND_PACKAGE_DIR_debug=${CND_DISTDIR}/debug/package
+CND_PACKAGE_NAME_debug=serialtest16f876axc8.x.tar
+CND_PACKAGE_PATH_debug=${CND_DISTDIR}/debug/package/serialtest16f876axc8.x.tar
+# release configuration
+CND_ARTIFACT_DIR_release=dist/release/production
+CND_ARTIFACT_NAME_release=serialtest_16f876a_xc8.X.production.hex
+CND_ARTIFACT_PATH_release=dist/release/production/serialtest_16f876a_xc8.X.production.hex
+CND_PACKAGE_DIR_release=${CND_DISTDIR}/release/package
+CND_PACKAGE_NAME_release=serialtest16f876axc8.x.tar
+CND_PACKAGE_PATH_release=${CND_DISTDIR}/release/package/serialtest16f876axc8.x.tar
---
+++ build/mplabx-pic18/serialtest_18f252_xc8.X/nbproject/Makefile-variables.mk
@@ -0,0 +1,20 @@
+#
+# Generated - do not edit!
+#
+# NOCDDL
+#
+CND_BASEDIR=`pwd`
+# debug configuration
+CND_ARTIFACT_DIR_debug=dist/debug/production
+CND_ARTIFACT_NAME_debug=serialtest_18f252_xc8.X.production.hex
+CND_ARTIFACT_PATH_debug=dist/debug/production/serialtest_18f252_xc8.X.production.hex
+CND_PACKAGE_DIR_debug=${CND_DISTDIR}/debug/package
+CND_PACKAGE_NAME_debug=serialtest18f252xc8.x.tar
+CND_PACKAGE_PATH_debug=${CND_DISTDIR}/debug/package/serialtest18f252xc8.x.tar
+# release configuration
+CND_ARTIFACT_DIR_release=dist/release/production
+CND_ARTIFACT_NAME_release=serialtest_18f252_xc8.X.production.hex
+CND_ARTIFACT_PATH_release=dist/release/production/serialtest_18f252_xc8.X.production.hex
+CND_PACKAGE_DIR_release=${CND_DISTDIR}/release/package
+CND_PACKAGE_NAME_release=serialtest18f252xc8.x.tar
+CND_PACKAGE_PATH_release=${CND_DISTDIR}/release/package/serialtest18f252xc8.x.tar
