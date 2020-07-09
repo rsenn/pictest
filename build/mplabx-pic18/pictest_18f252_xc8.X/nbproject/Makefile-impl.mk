31e1dfb44258f2798b0a80cdada0c44e2f79fb69
Roman <roman.l.senn@gmail.com>
2020-07-09 06:58:31 +0200

    ...

--- build/mplabx-pic18/pictest_18f252_xc8.X/nbproject/Makefile-impl.mk
+++ build/mplabx-pic18/pictest_18f252_xc8.X/nbproject/Makefile-impl.mk
@@ -24,14 +24,14 @@ CLEAN_SUBPROJECTS=${CLEAN_SUBPROJECTS_${SUBPROJECTS}}
 
 
 # Project Name
-PROJECTNAME=seg7test_18f25k50_xc8.X
+PROJECTNAME=pictest_18f252_xc8.X
 
 # Active Configuration
 DEFAULTCONF=debug
 CONF=${DEFAULTCONF}
 
 # All Configurations
-ALLCONFS=debug release 
+ALLCONFS=debug 
 
 
 # build
@@ -46,14 +46,12 @@ ALLCONFS=debug release
 # clobber
 .clobber-impl: .clobber-pre .depcheck-impl
 	    ${MAKE} SUBPROJECTS=${SUBPROJECTS} CONF=debug clean
-	    ${MAKE} SUBPROJECTS=${SUBPROJECTS} CONF=release clean
 
 
 
 # all
 .all-impl: .all-pre .depcheck-impl
 	    ${MAKE} SUBPROJECTS=${SUBPROJECTS} CONF=debug build
-	    ${MAKE} SUBPROJECTS=${SUBPROJECTS} CONF=release build
 
 
 
