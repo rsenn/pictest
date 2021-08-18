0d7d2efa63b2e8233204d9d12506bbf6813a7263
Roman Senn <roman.l.senn@gmail.com>
2021-08-19 00:39:23 +0200

    ...

--- build/mplabx-pic18/pictest_18f25k50_xc8.X/nbproject/Makefile-variables.mk
+++ build/mplabx-pic18/pictest_18f25k50_xc8.X/nbproject/Makefile-variables.mk
@@ -11,3 +11,10 @@ CND_ARTIFACT_PATH_debug=dist/debug/production/pictest_18f25k50_xc8.X.production.
 CND_PACKAGE_DIR_debug=${CND_DISTDIR}/debug/package
 CND_PACKAGE_NAME_debug=pictest18f25k50xc8.x.tar
 CND_PACKAGE_PATH_debug=${CND_DISTDIR}/debug/package/pictest18f25k50xc8.x.tar
+# release configuration
+CND_ARTIFACT_DIR_release=dist/release/production
+CND_ARTIFACT_NAME_release=pictest_18f25k50_xc8.X.production.hex
+CND_ARTIFACT_PATH_release=dist/release/production/pictest_18f25k50_xc8.X.production.hex
+CND_PACKAGE_DIR_release=${CND_DISTDIR}/release/package
+CND_PACKAGE_NAME_release=pictest18f25k50xc8.x.tar
+CND_PACKAGE_PATH_release=${CND_DISTDIR}/release/package/pictest18f25k50xc8.x.tar
--- build/mplabx-pic18/pictest_18f25k50_xc8.X/nbproject/Package-release.bash
+++ build/mplabx-pic18/pictest_18f25k50_xc8.X/nbproject/Package-release.bash
@@ -6,13 +6,13 @@
 
 # Macros
 TOP=`pwd`
-CND_CONF=debug
+CND_CONF=release
 CND_DISTDIR=dist
 TMPDIR=build/${CND_CONF}/${IMAGE_TYPE}/tmp-packaging
 TMPDIRNAME=tmp-packaging
-OUTPUT_PATH=dist/${CND_CONF}/${IMAGE_TYPE}/serialtest_18f25k50_xc8.X.${IMAGE_TYPE}.${OUTPUT_SUFFIX}
-OUTPUT_BASENAME=serialtest_18f25k50_xc8.X.${IMAGE_TYPE}.${OUTPUT_SUFFIX}
-PACKAGE_TOP_DIR=serialtest18f25k50xc8.x/
+OUTPUT_PATH=dist/${CND_CONF}/${IMAGE_TYPE}/pictest_18f25k50_xc8.X.${IMAGE_TYPE}.${OUTPUT_SUFFIX}
+OUTPUT_BASENAME=pictest_18f25k50_xc8.X.${IMAGE_TYPE}.${OUTPUT_SUFFIX}
+PACKAGE_TOP_DIR=pictest18f25k50xc8.x/
 
 # Functions
 function checkReturnCode
@@ -57,15 +57,15 @@ mkdir -p ${TMPDIR}
 
 # Copy files and create directories and links
 cd "${TOP}"
-makeDirectory ${TMPDIR}/serialtest18f25k50xc8.x/bin
+makeDirectory ${TMPDIR}/pictest18f25k50xc8.x/bin
 copyFileToTmpDir "${OUTPUT_PATH}" "${TMPDIR}/${PACKAGE_TOP_DIR}bin/${OUTPUT_BASENAME}" 0755
 
 
 # Generate tar file
 cd "${TOP}"
-rm -f ${CND_DISTDIR}/${CND_CONF}/package/serialtest18f25k50xc8.x.tar
+rm -f ${CND_DISTDIR}/${CND_CONF}/package/pictest18f25k50xc8.x.tar
 cd ${TMPDIR}
-tar -vcf ../../../../${CND_DISTDIR}/${CND_CONF}/package/serialtest18f25k50xc8.x.tar *
+tar -vcf ../../../../${CND_DISTDIR}/${CND_CONF}/package/pictest18f25k50xc8.x.tar *
 checkReturnCode
 
 # Cleanup
