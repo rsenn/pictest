0e208cc0e8c2374e3ebeb721fafd31f20cc800a2
Roman Senn <roman.l.senn@gmail.com>
2021-08-19 02:00:54 +0200

    ...

--- build/mplabx-pic18/blinktest_18f2550_sdcc.X/nbproject/Makefile-local-debug.mk
+++ build/mplabx-pic18/blinktest_18f2550_sdcc.X/nbproject/Makefile-local-debug.mk
@@ -14,11 +14,11 @@
 # You can invoke make with the values of the macros:
 # $ makeMP_CC="/opt/microchip/mplabc30/v3.30c/bin/pic30-gcc" ...  
 #
-PATH_TO_IDE_BIN=/opt/microchip/mplabx/v5.30/mplab_platform/platform/../mplab_ide/modules/../../bin/
+PATH_TO_IDE_BIN=/opt/microchip/mplabx/v5.50/mplab_platform/platform/../mplab_ide/modules/../../bin/
 # Adding MPLAB X bin directory to path.
-PATH:=/opt/microchip/mplabx/v5.30/mplab_platform/platform/../mplab_ide/modules/../../bin/:$(PATH)
+PATH:=/opt/microchip/mplabx/v5.50/mplab_platform/platform/../mplab_ide/modules/../../bin/:$(PATH)
 # Path to java used to run MPLAB X when this makefile was created
-MP_JAVA_PATH="/opt/microchip/mplabx/v5.30/sys/java/jre1.8.0_181/bin/"
+MP_JAVA_PATH="/opt/microchip/mplabx/v5.50/sys/java/jre1.8.0_181/bin/"
 OS_CURRENT="$(shell uname -s)"
 MP_CC="/opt/sdcc-3.9.0/bin/sdcc"
 # MP_CPPC is not defined
@@ -26,7 +26,7 @@ MP_CC="/opt/sdcc-3.9.0/bin/sdcc"
 # MP_AS is not defined
 # MP_LD is not defined
 # MP_AR is not defined
-DEP_GEN=${MP_JAVA_PATH}java -jar "/opt/microchip/mplabx/v5.30/mplab_platform/platform/../mplab_ide/modules/../../bin/extractobjectdependencies.jar"
+DEP_GEN=${MP_JAVA_PATH}java -jar "/opt/microchip/mplabx/v5.50/mplab_platform/platform/../mplab_ide/modules/../../bin/extractobjectdependencies.jar"
 MP_CC_DIR="/opt/sdcc-3.9.0/bin"
 # MP_CPPC_DIR is not defined
 # MP_BC_DIR is not defined
