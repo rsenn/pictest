5e8b11a42bcbd8e314e4b5d342de8cb1c159a4fd
Roman Senn <roman.l.senn@gmail.com>
2024-03-12 02:06:44 +0100

    ...

--- src/pwmtest.c
+++ src/pwmtest.c
@@ -39,15 +39,15 @@ volatile static uint8_t ticks;
 volatile static uint16_t poti;
 
 INTERRUPT_FN() {
-  if(TIMER0_INTERRUPT_FLAG) { // Did we get a timer0 interrupt?
+  if(T0IF) { // Did we get a timer0 interrupt?
 
     PWM_setduty(1, sine_table[index % ARRAY_SIZE(sine_table)]);
-    PWM_setduty(2, sine_table[(uint8_t)(index + 64) % ARRAY_SIZE(sine_table)]);
+    // PWM_setduty(2, sine_table[(uint8_t)(index + 64) % ARRAY_SIZE(sine_table)]);
 
     index++;
 
     TMR0 = 255 - ticks;
-    TIMER0_INTERRUPT_CLEAR();
+    T0IF = 0;
   }
 }
 
@@ -55,13 +55,17 @@ void
 main(void) {
   ticks = 0xff;
   index = 0;
-  CMCON = 0b111; /** Disable comparators.  NEEDED FOR NORMAL LATA
-                  *  BEHAVIOR ON PIC16f628a!
-                  */
+#if PIC16
+  CMCON = 0b111; /** Disable comparators.  NEEDED FOR NORMAL LATA BEHAVIOR ON PIC16f628a! */
+#endif
+#ifndef NO_PORTC
   TRISC2 = OUTPUT;
   TRISC1 = OUTPUT;
+  TRISC0 = OUTPUT;
+  RC0 = 0;
+#endif
 
-  adc_init();
+  adc_init(ADCS_FOSC_64, 0);
 
 #ifdef USE_HD44780_LCD
   lcd_init(1);
@@ -71,13 +75,14 @@ main(void) {
   pwm_init(0b00);
 
   PWM_start(1);
-  PWM_start(2);
+  // PWM_start(2);
 
   /*  PR2 = 0xff;  // Set PWM period
     CCP1M = 0b1100;  // Set PWM mode
     */
   PWM_setduty(1, 0);
-  PWM_setduty(2, 0);
+  // PWM_setduty(2, 0);
+
   /*CCP1X = 0;  // Set one of the LSB bits.
   CCP1Y = 0;
   CCPR1L = 0x00;  // Set PWM duty cycle*/
@@ -102,11 +107,10 @@ main(void) {
   T0IE = 1;
   TMR0 = 0;
 
+#if USE_LED
   LED_TRIS();
   LED_OFF();
-
-  TRISC0 = 0;
-  RC0 = 0;
+#endif
 
 #ifdef USE_HD44780_LCD
   lcd_clear();
